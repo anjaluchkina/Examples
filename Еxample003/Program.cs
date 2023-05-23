4360863c8119e685d528fe08c78e83c06a32164f
«Anna» <anjakabanova@yandex.ru>
2023-05-23 13:45:42 +0300

    Добавили Random

--- Еxample003/Program.cs
+++ Еxample003/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Написать программу сложения двух чисел
-int numberA = 5;
-int nimberB = 31;
-int resuil = numberA + nimberB;
+int numberA = new Random().Next(1, 10); //1 2 3 4 5 ... 9
+System.Console.Write(numberA);
+int numberB = new Random().Next(1, 10);
+System.Console.Write(numberB);
+int resuil = numberA + numberB;
 System.Console.WriteLine(resuil);
