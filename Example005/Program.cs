3b2683e5e53ccf92c14ffbb00222e532c27f994a
«Anna» <anjakabanova@yandex.ru>
2023-05-23 14:01:46 +0300

    Добавили папку 005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = System.Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    System.Console.WriteLine("Ура это же Маша!");
+}
+else
+{
+    System.Console.Write("Привет, ");
+    System.Console.WriteLine(username);
+}
